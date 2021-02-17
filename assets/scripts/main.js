cd731010429ec773bfce47173ba6d31808f09f1b
Quinn Ditto <qditto@sandstormdesign.com>
2021-02-16 22:07:34 -0600

    intiral commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,37 @@
+//Create a stage by getting a reference to the canvas
+
+function init() {
+    stage = new createjs.Stage("gameMain");
+
+    var backgroundImage = new createjs.Bitmap("./assets/imgs/bg.png")
+    stage.addChild(backgroundImage);
+    var shape1 = new createjs.Shape();
+    var shape2 = new createjs.Shape();
+    var shape3 = new createjs.Shape();
+    var shape4 = new createjs.Shape();
+
+    shape1.graphics.beginFill("#ff0000")
+    shape2.graphics.beginFill("#ff0")
+    shape3.graphics.beginFill("#222fff")
+    shape4.graphics.beginFill("#ff0fff")
+
+    let shapes = [shape1, shape2, shape3, shape4]
+
+    for (let i = 0; i < 4; i++){
+        console.log(shapes[i])
+        shapes[i].positionId = i
+        if (i == 0){
+            y = 95
+        } else {
+            y = 100 * (i + 1)
+        }
+        shapes[i].graphics.drawRect(4, y, 31, 20);
+        stage.addChild(shapes[i])
+
+    }
+
+    stage.update()
+    console.log(stage)
+}
+
+
